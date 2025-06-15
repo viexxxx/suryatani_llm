f5e560a775e10b6efc307497243a307f14a878a9
viexxxx <viqialvi11@gmail.com>
2025-06-16 00:59:37 +0700

    open ai embeddings

--- extract_knowledge.py
+++ extract_knowledge.py
@@ -3,6 +3,13 @@ import argparse
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
+from langchain.llms import OpenAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.callbacks import get_openai_callback
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--input", required=True)
@@ -24,5 +31,18 @@ text_splitter = CharacterTextSplitter(
 chunks = text_splitter.split_text(text)
 
 #embeddings
+print("[INFO] Embedding Texts")
 embeddings = OpenAIEmbeddings()
 knowledge_base = FAISS.from_texts(chunks, embeddings)
+
+#chat logic
+print("[INFO] Starting Bot ..")
+while True:
+    pertanyaan = input("Anda > ")
+    docs = knowledge_base.similarity_search(pertanyaan)
+
+    llm = OpenAI()
+    chain = load_qa_chain(llm, chain_type='stuff')
+    with get_openai_callback() as cb:
+        response = chain.run(input_documents=docs, question=pertanyaan)
+    print(response)
